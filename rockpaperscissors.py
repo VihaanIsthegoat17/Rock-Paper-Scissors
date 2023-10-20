e355ec66b9e572344cf0291f041a5c17113519c9
VihaanIsthegoat17 <148399070+VihaanIsthegoat17@users.noreply.github.com>
2023-10-20 17:55:02 -0400

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -8,39 +8,39 @@ while (1):
 
   GameList = ["Rock", "Paper", "Scissor"]
   #you have to like make coiches that can be picked and say if they won or not.
-  Vihaans_pick = gp.getpass(
+  Player1_pick = gp.getpass(
       prompt=
-      '\nVihaan,Please select rock,paper or scissors\n Enter Exit if you get bored\n'
+      '\nPlayer1,Please select rock,paper or scissors\n Enter Exit if you get bored\n'
   ).upper()  # u can type any rock, Rock, RoCk.etc...
-  #Virat gets to pick
-  virats_pick = gp.getpass(
+  #Player2 gets to pick
+  Player2s_pick = gp.getpass(
       prompt=  #getpass is a pass to use the thing
-      '\nvirat,Please select rock,paper or scissor\n Enter Exit if you get bored\n'
+      '\nPlayer2,Please select rock,paper or scissor\n Enter Exit if you get bored\n'
   ).upper()
 
-  if (Vihaans_pick == "Exit".upper()):
+  if (Player1s_pick == "Exit".upper()):
     print("Game Over")
     break  #for this it is if i pick exit then the game is over and we take a break and it will display game over
-  print(Vihaans_pick, virats_pick)
+  print(Player1s_pick, Player2s_pick)
 
-  if (Vihaans_pick == virats_pick):
-    print("Tie")  #this is if me and virat pick the same thing it will say tie
+  if (Player1s_pick == Player2s_pick):
+    print("Tie")  #this is if me and Player2 pick the same thing it will say tie
   elif (
-      Vihaans_pick == "Rock".upper()
+      Player1s_pick == "Rock".upper()
   ):  #we made upper because then even if you type rock in lowercase it will auto =maticly make it uppercase.
-    if (virats_pick == "Scissor".upper()):
-      print("Vihaan wins!!!Hurray!!!!Yay!!!")
-    if (virats_pick == "Paper".upper()): 
-      print("virat wins"):
-  elif (Vihaans_pick == "Paper".upper()):
-    if (virats_pick == "Scissor".upper()):
-      print("virat wins!!!!")
-    if (virats_pick == "Rock".upper()):
-      print("Vihaan wins!!Excellent!!")
+    if (Player2s_pick == "Scissor".upper()):
+      print("Player1 wins!!!Hurray!!!!Yay!!!")
+    if (Player2s_pick == "Paper".upper()): 
+      print("Player2 wins"):
+  elif (Player1s_pick == "Paper".upper()):
+    if (Player2s_pick == "Scissor".upper()):
+      print("Player2 wins!!!!")
+    if (Player2s_pick == "Rock".upper()):
+      print("Player1 wins!!Excellent!!")
 
-  elif (Vihaans_pick == "Scissor".upper()):
-    if (virats_pick == "Rock".upper()):
-      print("virat wins!"):
-    if (virats_pick == "Paper"):
-      print("Vihaan!!The winner!!")
+  elif (Player1s_pick == "Scissor".upper()):
+    if (Player2s_pick == "Rock".upper()):
+      print("Player2 wins!"):
+    if (Player2s_pick == "Paper"):
+      print("Player1!!The winner!!")
   print("\nStart new round")  #and this is to start a new round
